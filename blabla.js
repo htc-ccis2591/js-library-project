997fd45bf089214d875fc0fa6dc74ffec092963b
pyasger <pyasger@gmail.com>
2016-11-16 16:14:49 -0600

    WIP

---
+++ blabla.js
@@ -0,0 +1,55 @@
+var video = videojs('my-video');
+
+
+/*the video was working.  Now getting the librarys to connect to the video or will have 4 or 5 videos.  in the middle of the process of changing over.  If having all of the features in one player, then i'll have this.  If not, then i'll have seperate videos for each.*/
+
+/* 
+1./three second time out/
+
+video.ready(function () {
+
+setTimeout(() => {
+
+        this.play();
+}, 3000);
+
+})
+*/
+
+/*
+
+2./video notifying that the video ended/
+
+video.on('ended', function () {
+
+        alert('The video has ended.');
+*/
+
+
+/*
+
+3./this showes how much time is left/
+
+video.on('pause', function(){
+        alert('You have ' + this.remainingTime() + ' second left to watch.');
+});
+
+/*
+
+4./this alerts the video is paused/
+
+video.on('pause', function () {
+alert('The video has been .');
+
+});
+
+
+
+
+
+*/
+
+
+
+* /
+});
