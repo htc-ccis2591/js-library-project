dbd7b4172e4717ae255e720d92bdacb3bc1695a9
kmbrandt94 <kmbrandt94@gmail.com>
2016-11-30 20:38:49 -0600

    Javascript Library WIP

---
+++ script.js
@@ -0,0 +1,10 @@
+$(function () {
+
+    var verticalBounce = anime({
+        targets: '.ball',
+        translateY: '50vh',
+        duration: 500,
+        loop: true,
+        direction: 'alternate',
+    });
+});
